953d4290cd4582fd54cfd898e1ce941c8e65c2a4
payamzowghi <pzoughi@gmail.com>
2017-04-16 00:04:37 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "remote_state" {
-  bucket = "${var.prefix}-remote_state-${var.environment}"
+  bucket = "${var.prefix}_remote_state_${var.environment}"
   acl    = "authenticated-read"
  
   versioning {
